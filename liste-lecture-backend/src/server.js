c7cbb9aa2585be36e123f25ce02cec2068963ba9
Alain Filion <filion_alain@hotmail.com>
2022-12-05 20:21:40 -0500

    Ajout des routes du backend, fonctionne avec MongoDB

--- liste-lecture-backend/src/server.js
+++ liste-lecture-backend/src/server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 
-const app = express();
+import { getPiece } from './bd/getPiece.js';
+import { getPieces } from './bd/getPieces.js';
+import { ajouterPiece } from './bd/ajouterPiece.js';
+import { supprimerPiece } from './bd/supprimerPiece.js';
+import { modifierPiece } from './bd/modifierPiece.js';
 
-app.get('/hello', (requete, reponse) => {
-    reponse.send("Hello world");
-})
+const app = express();
+app.use(express.json());
+
+app.get('/api/pieces', getPieces);
+app.get('/api/pieces/:id', getPiece);
+app.post('/api/pieces/ajouter', ajouterPiece);
+app.put('/api/pieces/modifier/:id', modifierPiece);
+app.delete('/api/pieces/supprimer/:id', supprimerPiece);
 
 app.listen(8000, () => console.log('Backend démarré sur le port 8000'));
