838216ecdae4e2f9da62e8cb6a7debbdca437e95
Alain Filion <filion_alain@hotmail.com>
2022-12-07 20:10:29 -0500

    Extraction de FormulairePiece à partir de deux formulaires semblables (DRY)

--- liste-lecture-frontend/src/composants/FormulaireAjouterPiece.js
+++ liste-lecture-frontend/src/composants/FormulaireAjouterPiece.js
@@ -2,18 +2,14 @@ import {
     React,
     useState
 } from 'react';
-import { Form } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
 import { Navigate } from "react-router-dom";
+
+import { FormulairePiece } from './FormulairePiece.js';
 
 export const FormulaireAjouterPiece = () => {
     const [titre, setTitre] = useState('');
     const [artiste, setArtiste] = useState('');
     const [categorie, setCategorie] = useState('');
-
-    const [titreEstPresent, setTitreEstPresent] = useState(true);
-    const [artisteEstPresent, setArtisteEstPresent] = useState(true);
-    const [categorieEstPresent, setCategorieEstPresent] = useState(true);
 
     const [rediriger, setRediriger] = useState(false);
 
@@ -27,57 +23,16 @@ export const FormulaireAjouterPiece = () => {
         });
         setRediriger(true);
     };
-
-    function validerFormulaire() {
-        setTitreEstPresent(titre !== "");
-        setArtisteEstPresent(artiste !== "");
-        setCategorieEstPresent(categorie !== "");
-
-        if ((titre !== "") && (artiste !== "") && (categorie !== "")) {
-            envoyerFormulaire();
-        }
-    }
     
     return (
     <>
         { rediriger ? <Navigate to="/admin" /> : null }
-        <Form className="mb-1">
-            <Form.Group>
-                <Form.Label htmlFor="inputTitre">Titre
-                    {titreEstPresent === false ? 
-                        <span className="text-danger"> * Vous devez entrer un titre.</span>
-                        : undefined
-                    }</Form.Label>
-                <Form.Control type="text" value={titre} id="inputTitre"
-                    onChange={(event) => setTitre(event.target.value)} />
-            </Form.Group>
-
-            <Form.Group>
-                <Form.Label htmlFor="inputArtiste">Artiste / Groupe
-                    {artisteEstPresent === false ? 
-                        <span className="text-danger"> * Vous devez entrer un nom d'artiste.</span>
-                        : undefined
-                    }
-                </Form.Label>
-                <Form.Control type="text" value={artiste} id="inputArtiste"
-                    onChange={(event) => setArtiste(event.target.value)} />
-            </Form.Group>
-
-            <Form.Group>
-                <Form.Label htmlFor="inputCategorie">Catégorie
-                    {categorieEstPresent === false ? 
-                        <span className="text-danger"> * Vous devez entrer une catégorie.</span>
-                        : undefined
-                    }
-                </Form.Label>
-                <Form.Control type="text" value={categorie} id="inputCategorie"
-                    onChange={(event) => setCategorie(event.target.value)} />
-            </Form.Group>
-
-            <Button className="mt-2" variant="primary" onClick={validerFormulaire} >
-                Ajouter
-            </Button>
-        </Form>
+        <FormulairePiece 
+            titre={titre} setTitre={setTitre}
+            artiste={artiste} setArtiste={setArtiste}
+            categorie={categorie} setCategorie={setCategorie}
+            texteBouton={"Ajouter"} fonctionEnvoyer={envoyerFormulaire}
+        />
     </>
     );
 }
